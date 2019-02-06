bedd7b0efecd80b0fab46e60d0553ee12656cf60
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2019-02-06 09:56:02 -0800

    Update test app

--- load_balance_nginx/was/install_app.py
+++ load_balance_nginx/was/install_app.py
@@ -3,5 +3,5 @@ serverName = AdminServerManagement.listServers()[0]
 serverName = serverName[:serverName.find("(")]
 AdminApplication.uninstallApplication("DefaultApplication")
 # https://www.ibm.com/support/knowledgecenter/en/SSAW57_9.0.0/com.ibm.websphere.nd.multiplatform.doc/ae/rxml_taskoptions.html
-AdminApp.install("/work/java_hello_world.ear", ["-appname", "java_hello_world", "-node", nodeName, "-server", serverName, "-usedefaultbindings"])
+AdminApp.install("/work/java_web_hello_world.ear", ["-appname", "java_web_hello_world", "-node", nodeName, "-server", serverName, "-usedefaultbindings"])
 AdminConfig.save()
